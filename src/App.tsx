010de83753bd6cdb13bbdbea140c18b3ed586660
Yuriy Stepin <y.stepin@x5.ru>
2023-11-12 21:16:28 +0100

    display userData

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,30 @@
 import {useEffect, useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-    const [userData, setUserData] = useState(null)
-    useEffect(()=>{
-              // Получение данных пользователя из initDat
-             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-expect-error
-        const initData = window.Telegram?.WebApp?.initData;
-        setUserData(initData);
-    },[])
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-          <pre>{userData && JSON.stringify(userData,null,2)}</pre>
-      </div>
-    </>
-  )
+const App = () => {
+    const [user, setUser] = useState<TelegramWebAppUserData | null>(null);
+
+    useEffect(() => {
+        if (window.Telegram) {
+            const initData = window.Telegram.initData;
+            setUser(initData.user);
+        }
+    }, []);
+
+    if (!user) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div>
+            <h1>User Information</h1>
+            <p>ID: {user.id}</p>
+            <p>Name: {user.first_name} {user.last_name}</p>
+            <p>Username: {user.username || 'Not provided'}</p>
+            <p>Photo URL: {user.photo_url ? <img src={user.photo_url} alt="User" /> : 'No photo'}</p>
+        </div>
+    );
 }
+
 
 export default App
