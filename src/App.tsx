b9f67e6b3d2d068926e32d5362e0e9e0a9527e5a
Yuriy Stepin <y.stepin@x5.ru>
2023-11-12 22:07:40 +0100

    add webb app api 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React, { useEffect, useState } from 'react';
 
 const App:React.FC = () => {
-    const [user, setUser] = useState<TelegramWebAppUserData | null>(null);
-
+    const [data, setData] = useState<TelegramWebAppInitData | null>(null)
     useEffect(() => {
         if (window.Telegram?.WebApp) {
             const initDataUnsafe = window.Telegram.WebApp.initDataUnsafe
-            setUser(initDataUnsafe.user);
+            setData(initDataUnsafe)
         }
     }, []);
-
+    const user = data?.user
     if (!user) {
         return <div>Loading...</div>;
     }
-
     return (
         <div>
-            <h1>User Information</h1>
+            <h2>User Information</h2>
             <p>ID: {user.id}</p>
             <p>Name: {user.first_name} {user.last_name}</p>
             <p>Username: {user.username ?? 'Not provided'}</p>
             <p>Photo URL: {user.photo_url ? <img src={user.photo_url} alt="User" /> : 'No photo'}</p>
+            <pre>{JSON.stringify(data,null,2)}</pre>
         </div>
     );
 }
