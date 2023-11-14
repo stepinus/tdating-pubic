d018d6d425f73ab4635705eec8873e07f3d964aa
Yuriy Stepin <y.stepin@x5.ru>
2023-11-14 23:42:14 +0100

    swipe v3.2

--- src/SwipeCards.tsx
+++ src/SwipeCards.tsx
@@ -1,15 +1,22 @@
 import React, { useState } from 'react';
-import { useSprings, animated, interpolate } from 'react-spring';
+import { useSprings, animated } from 'react-spring';
 import { useGesture } from 'react-use-gesture';
 import CardComponent from './CardComponent'; // Убедитесь, что этот компонент импортирован
 import { testData } from './testData'; // Данные для карточек
 
-const to = (i:number) => ({ x: 0, y: i * -4, scale: 1, rot: -10 + Math.random() * 20, delay: i * 100 });
-const from = () => ({ x: 0, rot: 0, scale: 1.5, y: -1000 });
-const trans = (r:number, s:number) => `perspective(1500px) rotateX(30deg) rotateY(${r / 10}deg) rotateZ(${r}deg) scale(${s})`;
+// interface CardData {
+//     imageUrl: string;
+//     name: string;
+//     age: number;
+//     hobby: string;
+// }
 
-const SwypeCards:React.FC = () => {
-    const [gone] = useState(() => new Set());
+const to = (i: number) => ({ x: 0, y: 0, scale: 1, rot: 0, delay: i * 100 });
+const from = () => ({ x: 0, rot: 0, scale: 1.5, y: -1000 });
+const trans = (_r: number, s: number) => `perspective(1500px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scale(${s})`;
+
+const SwipeCards:React.FC = ()=> {
+    const [gone] = useState(() => new Set<number>());
     const [props, set] = useSprings(testData.length, i => ({ ...to(i), from: from() }));
 
     const bind = useGesture({
@@ -25,13 +32,14 @@ const SwypeCards:React.FC = () => {
                 const scale = down ? 1.1 : 1;
                 return { x, rot, scale, delay: undefined, config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 } };
             });
-            if (!down && gone.size === testData.length) setTimeout(() => {gone.clear(); set(i => to(i))}, 600);
+            if (!down && gone.size === testData.length) setTimeout(() => {gone.clear() ; set(i => to(i))}, 600);
         }
     });
 
-    return props.map(({ x, y, rot, scale }, i) => (
-        <animated.div key={i} style={{ transform: interpolate([x, y], (x, y) => `translate3d(${x}px,${y}px,0)`) }}>
-            <animated.div {...bind(i)} style={{ transform: interpolate([rot, scale], trans) }}>
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    return props.map(({ x, y,rot, scale }, i) => (
+        <animated.div key={`${i}${rot}`} style={{ transform: x.to(x => `translate3d(${x}px,${y}px,0)`) }}>
+            <animated.div {...bind(i)} style={{ transform: scale.to(trans) }}>
                 <CardComponent
                     imageUrl={testData[i].imageUrl}
                     name={testData[i].name}
@@ -42,4 +50,5 @@ const SwypeCards:React.FC = () => {
         </animated.div>
     ));
 }
-export default SwypeCards;
+
+export default SwipeCards;
