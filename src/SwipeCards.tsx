7e49fea335fba6081415d45daba6dc7c98858c52
Yuriy Stepin <y.stepin@x5.ru>
2023-11-16 00:30:53 +0100

    swipe v3.6 perfomance fix

--- src/SwipeCards.tsx
+++ src/SwipeCards.tsx
@@ -6,19 +6,19 @@ import 'swiper/css';
 import 'swiper/css/virtual';
 import 'swiper/css/effect-cards';
 import { EffectCards } from 'swiper/modules';
+import {testData} from "./testData.ts";
+import CardComponent from "./CardComponent.tsx";
 
 export default () => {
     // Create array with 1000 slides
-    const slides = Array.from({ length: 1000 }).map(
-        (_el, index) => `Slide ${index + 1}`
-    );
+
 
     return (
         <Swiper modules={[Virtual,EffectCards]}   effect={'cards'}
                 grabCursor={true} spaceBetween={50} slidesPerView={3} virtual>
-            {slides.map((slideContent, index) => (
-                <SwiperSlide key={slideContent} virtualIndex={index}>
-                    {slideContent}
+            {testData.map((slideContent, index) => (
+                <SwiperSlide key={slideContent.name} virtualIndex={index}>
+                  <CardComponent {...slideContent}/>
                 </SwiperSlide>
             ))}
         </Swiper>
